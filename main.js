22eb65ac0ecab578242033e9ab0862bbd92bf946
kwonjintaik <kwonjintaik@gmail.com>
2026-03-22 05:50:36 +0000

    Update product builder project files

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-// Add JS here
+const generatorBtn = document.getElementById('generator-btn');
+const lottoNumbersContainer = document.getElementById('lotto-numbers');
+
+generatorBtn.addEventListener('click', () => {
+    lottoNumbersContainer.innerHTML = '';
+    const numbers = new Set();
+    while (numbers.size < 6) {
+        numbers.add(Math.floor(Math.random() * 45) + 1);
+    }
+
+    for (const number of numbers) {
+        const circle = document.createElement('div');
+        circle.classList.add('lotto-number');
+        circle.textContent = number;
+        lottoNumbersContainer.appendChild(circle);
+    }
+});
